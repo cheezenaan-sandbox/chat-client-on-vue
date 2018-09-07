f21dd2b4054da135f225e091e05f986e79bb48c8
cheezenaan <cheezenaan@gmail.com>
2018-09-07 13:27:06 +0900

    lint(js): Apply prettier

--- config/serve.config.js
+++ config/serve.config.js
@@ -6,12 +6,12 @@ const serveOptions = {
   host: '0.0.0.0',
   port: 8080,
   hotClient: {
-    port: 8081
-  }
+    port: 8081,
+  },
 };
 
 module.exports = {
   ...config,
   mode: 'development',
-  serve: serveOptions
+  serve: serveOptions,
 };
--- config/webpack.config.js
+++ config/webpack.config.js
@@ -3,27 +3,27 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const isProduction = process.env.NODE_ENV === 'production';
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
-  template: path.join(__dirname, '../src/index.html')
+  template: path.join(__dirname, '../src/index.html'),
 });
 
 /** @type webpack.Configuration */
 const config = {
   mode: process.env.NODE_ENV || 'development',
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.js'],
   },
   entry: {
-    bundle: './src/javascripts/index.ts'
+    bundle: './src/javascripts/index.ts',
   },
   output: {
     path: path.join(__dirname, '../dist'),
-    filename: '[name].js'
+    filename: '[name].js',
   },
   module: {
-    rules: [{ test: /.ts$/, loader: 'ts-loader' }]
+    rules: [{ test: /.ts$/, loader: 'ts-loader' }],
   },
   plugins: [htmlWebpackPlugin],
-  devtool: isProduction ? 'eval' : 'cheap-module-source-map'
+  devtool: isProduction ? 'eval' : 'cheap-module-source-map',
 };
 
 module.exports = config;
